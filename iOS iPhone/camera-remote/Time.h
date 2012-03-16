90589e60f01a31b3dfa3dd96cd87980263b3a4e5
Kevin Harrington <Kevin@Kevin-Harringtons-MacBook-Pro.local>
2012-03-15 20:39:19 -0600

    adding timer

---
+++ iOS iPhone/camera-remote/Time.h
@@ -0,0 +1,37 @@
+//
+//  Time.h
+//  camera-remote
+//
+//  Created by Kevin Harrington on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Time : NSObject
+
+@property (nonatomic) NSTimeInterval totalTimeInSeconds;
+@property (nonatomic) int hours;
+@property (nonatomic) int minutes;
+@property (nonatomic) int seconds;
+
+- (void) setTime:hours:(int)_hours minutes:(int)_minutes seconds:(int)_seconds;
+
+/**
+ * @return NSString is format on @"hh:mm"
+ */
+- (NSString *) toStringDownToMinutes;
+
+/**
+ * @return NSString is format on @"hh:mm:ss"
+ */
+- (NSString *) toStringDownToSeconds;
+
+- (void) decrementSecond;
+
+
+
+
+
+
+@end 
---
+++ iOS iPhone/camera-remote/Time.m
@@ -0,0 +1,89 @@
+//
+//  Time.m
+//  camera-remote
+//
+//  Created by Kevin Harrington on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Time.h"
+
+@interface Time()
+ -(void) timeComponentsChanged;
+@end
+
+
+@implementation Time
+
+@synthesize totalTimeInSeconds = _totalTimeInSeconds;
+@synthesize hours = _hours;
+@synthesize minutes = _minutes;
+@synthesize seconds = _seconds;
+
+
+-(id) init {
+    NSLog(@"init");
+    return self;
+}
+
+-(void) setTotalTimeInSeconds:(NSTimeInterval)totalTimeInSeconds {
+    NSLog(@"Set total time in seconds: %f", totalTimeInSeconds);
+    _totalTimeInSeconds = (int)totalTimeInSeconds;
+    NSLog(@"Set total time in seconds: %f", self.totalTimeInSeconds);
+    [self timeComponentsChanged];
+    
+}
+
+-(void) timeComponentsChanged {
+    _hours = (int)(self.totalTimeInSeconds / 3600);
+    _minutes = (self.totalTimeInSeconds - self.hours * 3600) / 60; 
+    _seconds = self.totalTimeInSeconds - self.hours * 3600 - self.minutes * 60;
+    NSLog(@"updated time %i h %i m %i s", _hours, _minutes, _seconds);
+    NSLog(@"updated time %i h %i m %i s", self.hours, self.minutes, self.seconds);
+
+}
+
+- (void) setTime:hours:(int)__hours minutes:(int)__minutes seconds:(int)__seconds {
+    NSLog(@"Time changed");
+    self.totalTimeInSeconds = __hours * 3600 + __minutes * 60 + __seconds;
+}
+
+- (void) decrementSecond {
+    [self setTotalTimeInSeconds:_totalTimeInSeconds - 1];
+}
+
+- (NSString *) toStringDownToMinutes {
+    int hours = self.hours;
+    int minutes = self.minutes;
+    NSString *parsedTime;
+    if(hours < 10 && minutes < 10) 
+        parsedTime = [[NSString alloc]initWithFormat:@"0%i:0%i", hours, minutes];
+    else if(hours < 10)
+        parsedTime = [[NSString alloc]initWithFormat:@"0%i:%i", hours, minutes];
+    else if(minutes < 10)
+        parsedTime = [[NSString alloc]initWithFormat:@"%i:0%i", hours, minutes];
+    else
+        parsedTime = [[NSString alloc]initWithFormat:@"%i:%i", hours, minutes];
+    
+    NSLog(@"Returning this time %@", parsedTime);
+    return parsedTime;
+}
+
+- (NSString *) toStringDownToSeconds {
+    NSLog(@"STring conversions");
+    NSString * seconds;
+    if(self.seconds < 10) {
+        seconds = [[NSString alloc]initWithFormat:@":0%i", self.seconds];
+    }
+    else {
+        seconds = [[NSString alloc]initWithFormat:@":%i", self.seconds];
+    }
+    
+    NSString * assembledTime = [[NSString alloc]initWithFormat:@"%@%@",[self toStringDownToMinutes], seconds];
+    NSLog(@"Returning this time %@", assembledTime);
+
+    return assembledTime;
+}
+
+
+@end

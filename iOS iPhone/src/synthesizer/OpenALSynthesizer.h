bd24c540c313f22ec8c28b96b3b63a309e56f00f
Kevin Harrington <kevintharrington@gmail.com>
2012-10-12 18:18:36 -0600

    Adding missing images to remote

---
+++ iOS iPhone/src/synthesizer/OpenALSynthesizer.h
@@ -0,0 +1,37 @@
+//
+//  OpenALSynthesizer.h
+//  OpenALSynthesizer
+//
+//  Created by Kevin Harrington on 10/12/12.
+//  Copyright (c) 2012 Kevin Harrington. All rights reserved.
+//
+
+#import <OpenAL/al.h>
+#import <OpenAL/alc.h>
+
+@interface OpenALSynthesizer : NSObject {
+	ALCcontext* mContext;
+	ALCdevice* mDevice;
+	NSMutableDictionary *soundDictionary;
+	NSMutableArray *bufferStorageArray;
+}
+
+-(id) init;
+-(void) loadFile:(NSString *)soundName doesLoop:(BOOL)loops;
+-(void) playSound:(NSString*)soundKey doesLoop:(BOOL)loops;
+//-(void) playSound:(NSString*)soundKey withLoops:(int)loops;
+-(void) pauseAllSounds;
+-(void) resumeAllSounds;
+-(void) pauseSound:(NSString*)soundKey;
+
+-(void) stopAllSounds;
+-(void) loadFile:(NSString *)soundName withKey:(NSString*)key doesLoop:(BOOL)loops;
+
+
+
+@property(nonatomic, copy) NSMutableDictionary	*soundDictionary;
+@property(nonatomic, copy) NSMutableDictionary	*bufferDictionary;
+@property(nonatomic, copy) NSMutableArray		*bufferStorageArray;
+@property(nonatomic) BOOL hardwareConnected;
+
+@end

4b21da35b892a16403f121e2310b6b9111815fb4
Kevin Harrington <Kevin@Kevins-MacBook-Pro.local>
2013-06-30 16:12:25 -0700

    Adding Xcode Trigger Happy 2.0

---
+++ Trigger Happy/Trigger Happy/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Trigger Happy
+//
+//  Created by Kevin Harrington on 6/30/13.
+//  Copyright (c) 2013 Trigger Happy, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Trigger Happy/Trigger HappyTests/Trigger_HappyTests.m
@@ -0,0 +1,36 @@
+//
+//  Trigger_HappyTests.m
+//  Trigger HappyTests
+//
+//  Created by Kevin Harrington on 6/30/13.
+//  Copyright (c) 2013 Trigger Happy, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Trigger_HappyTests : XCTestCase
+
+@end
+
+@implementation Trigger_HappyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
